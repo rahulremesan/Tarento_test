655dcd466523f252bc8dc74d19dde103dddbf3ee
rahul3remesh <rahul3remesh@gmail.com>
2023-07-18 11:31:56 +0530

    clock updated

--- frontEndTask/index.js
+++ frontEndTask/index.js
@@ -32,4 +32,48 @@ const api = {
     weather_el.innerText = weather.weather[0].main;
   
   }
-  
+
+
+// Background changes as day and Night   
+  var currentTime = new Date().getHours();
+  if (document.body) {
+      if (7 <= currentTime && currentTime < 20) {
+          document.body.background = "https://images.unsplash.com/photo-1487621167305-5d248087c724?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80";
+      }
+      else {
+          document.body.background = "https://wallpaperaccess.com/full/2875716.jpg";
+      }
+  }
+
+
+
+  function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("digital-clock").innerText = time;
+    document.getElementById("digital-clock").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
+
